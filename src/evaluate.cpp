418ffd5224fbc0bb7da08ffd1e2caee8197aa85d
Chaitanya Chaudhary <chaitanyachaudhary30@gmail.com>
2026-06-29 20:12:14 +0530

    Add basic piece-square table evaluation

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -55,6 +55,129 @@ int pieceTypeValue(PieceType pieceType) {
     }
 }
 
+// Mirrors square vertically for black pieces.
+// Since Square::A1 = 0 and Square::A8 = 56, xor 56 flips ranks.
+static int mirrorSquareIndex(int sqIndex) {
+    return sqIndex ^ 56;
+}
+
+// Basic piece-square tables from White's perspective.
+// Index 0 = a1, index 63 = h8.
+
+// Pawns like advancing and controlling central files.
+
+static const int PAWN_SQUARE_BONUS[64] = {
+      0,   0,   0,   0,   0,   0,   0,   0,
+      5,  10,  10, -20, -20,  10,  10,   5,
+      5,  -5, -10,   0,   0, -10,  -5,   5,
+      0,   0,   0,  20,  20,   0,   0,   0,
+      5,   5,  10,  25,  25,  10,   5,   5,
+     10,  10,  20,  30,  30,  20,  10,  10,
+     50,  50,  50,  50,  50,  50,  50,  50,
+      0,   0,   0,   0,   0,   0,   0,   0
+};
+
+// Knights are best near the center and bad on edges/corners.
+static const int KNIGHT_SQUARE_BONUS[64] = {
+    -50, -40, -30, -30, -30, -30, -40, -50,
+    -40, -20,   0,   5,   5,   0, -20, -40,
+    -30,   5,  10,  15,  15,  10,   5, -30,
+    -30,   0,  15,  20,  20,  15,   0, -30,
+    -30,   5,  15,  20,  20,  15,   5, -30,
+    -30,   0,  10,  15,  15,  10,   0, -30,
+    -40, -20,   0,   0,   0,   0, -20, -40,
+    -50, -40, -30, -30, -30, -30, -40, -50
+};
+
+// Bishops like open diagonals and central squares.
+static const int BISHOP_SQUARE_BONUS[64] = {
+    -20, -10, -10, -10, -10, -10, -10, -20,
+    -10,   5,   0,   0,   0,   0,   5, -10,
+    -10,  10,  10,  10,  10,  10,  10, -10,
+    -10,   0,  10,  10,  10,  10,   0, -10,
+    -10,   5,   5,  10,  10,   5,   5, -10,
+    -10,   0,   5,  10,  10,   5,   0, -10,
+    -10,   0,   0,   0,   0,   0,   0, -10,
+    -20, -10, -10, -10, -10, -10, -10, -20
+};
+
+// Rooks like open/semi-open ranks/files, but this simple table mainly rewards activity.
+static const int ROOK_SQUARE_BONUS[64] = {
+      0,   0,   5,  10,  10,   5,   0,   0,
+     -5,   0,   0,   0,   0,   0,   0,  -5,
+     -5,   0,   0,   0,   0,   0,   0,  -5,
+     -5,   0,   0,   0,   0,   0,   0,  -5,
+     -5,   0,   0,   0,   0,   0,   0,  -5,
+     -5,   0,   0,   0,   0,   0,   0,  -5,
+      5,  10,  10,  10,  10,  10,  10,   5,
+      0,   0,   0,   5,   5,   0,   0,   0
+};
+
+// Queens get small centralization bonuses.
+static const int QUEEN_SQUARE_BONUS[64] = {
+    -20, -10, -10,  -5,  -5, -10, -10, -20,
+    -10,   0,   5,   0,   0,   0,   0, -10,
+    -10,   5,   5,   5,   5,   5,   0, -10,
+      0,   0,   5,   5,   5,   5,   0,  -5,
+     -5,   0,   5,   5,   5,   5,   0,  -5,
+    -10,   0,   5,   5,   5,   5,   0, -10,
+    -10,   0,   0,   0,   0,   0,   0, -10,
+    -20, -10, -10,  -5,  -5, -10, -10, -20
+};
+
+// Basic king table for middlegame: safer near back rank/corners.
+static const int KING_SQUARE_BONUS[64] = {
+     20,  30,  10,   0,   0,  10,  30,  20,
+     20,  20,   0,   0,   0,   0,  20,  20,
+    -10, -20, -20, -20, -20, -20, -20, -10,
+    -20, -30, -30, -40, -40, -30, -30, -20,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30
+};
+
+static int pieceSquareBonus(Piece piece, int sqIndex) {
+    Color color = pieceColor(piece);
+
+    // PSTs are written from White's perspective.
+    // For black pieces, mirror the square so black gets equivalent bonuses.
+    int tableIndex = (color == Color::White) ? sqIndex : mirrorSquareIndex(sqIndex);
+
+    switch (piece) {
+        case Piece::WhitePawn:
+        case Piece::BlackPawn:
+            return PAWN_SQUARE_BONUS[tableIndex];
+
+        case Piece::WhiteKnight:
+        case Piece::BlackKnight:
+            return KNIGHT_SQUARE_BONUS[tableIndex];
+
+        case Piece::WhiteBishop:
+        case Piece::BlackBishop:
+            return BISHOP_SQUARE_BONUS[tableIndex];
+
+        case Piece::WhiteRook:
+        case Piece::BlackRook:
+            return ROOK_SQUARE_BONUS[tableIndex];
+
+        case Piece::WhiteQueen:
+        case Piece::BlackQueen:
+            return QUEEN_SQUARE_BONUS[tableIndex];
+
+        case Piece::WhiteKing:
+        case Piece::BlackKing:
+            return KING_SQUARE_BONUS[tableIndex];
+
+        default:
+            return 0;
+    }
+}
+
+
+/*
+This evaluate function was till version 0.2.3
+
 int evaluate(const Position& pos) {
     int score = 0;
 
@@ -84,4 +207,39 @@ int evaluate(const Position& pos) {
     }
 }
 
+}
+*/
+
+
+// NOW THE BELOW EVALUATE WILL WORK FOR 0.2.4.
+int evaluate(const Position& pos) {
+    int score = 0;
+
+    for (int sqIndex = 0; sqIndex < 64; sqIndex++) {
+        Square sq = static_cast<Square>(sqIndex);
+
+        if (!pos.isOccupied(sq)) {
+            continue;
+        }
+
+        Piece piece = pos.pieceAt(sq);
+
+        int value = pieceValue(piece);
+        value += pieceSquareBonus(piece, sqIndex);
+
+        if (pieceColor(piece) == Color::White) {
+            score += value;
+        } 
+        else if (pieceColor(piece) == Color::Black) {
+            score -= value;
+        }
+    }
+
+    // Return score from side-to-move's perspective for negamax.
+    if (pos.sideToMove == Color::White) {
+        return score;
+    }
+
+    return -score;
+}
 }
